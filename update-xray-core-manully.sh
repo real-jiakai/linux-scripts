34ba1b350065fde0b1c0679bd85354e430e7a006
Jaya <gujiakai28@gmail.com>
2024-02-26 21:28:56 +0800

    Update update-xray-core-manully.sh

--- update-xray-core-manully.sh
+++ update-xray-core-manully.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-# 转到/usr/local/x-ui目录
-cd /usr/local/x-ui
+# 定义目标目录
+TARGET_DIR="/usr/local/x-ui/bin"
+XRAY_ZIP="${TARGET_DIR}/Xray-linux-64.zip"
 
-# 下载Xray-core的ZIP文件
-curl -O https://gh-hk.gujiakai.top/https://github.com/XTLS/Xray-core/releases/download/v1.8.8/Xray-linux-64.zip
+# 确保目标目录存在
+mkdir -p $TARGET_DIR
+
+# 获取最新的Xray-core发布的ZIP文件URL
+XRAY_LATEST_RELEASE_URL=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep browser_download_url | grep 'Xray-linux-64.zip' | cut -d '"' -f 4)
+
+if [ -z "$XRAY_LATEST_RELEASE_URL" ]; then
+  echo "无法获取Xray最新版本的下载链接。"
+  exit 1
+fi
+
+# 下载最新版本的Xray-core
+echo "下载Xray最新版本..."
+curl -L $XRAY_LATEST_RELEASE_URL -o $XRAY_ZIP
 
 # 解压ZIP文件
-unzip -o Xray-linux-64.zip
+echo "解压Xray..."
+unzip -o $XRAY_ZIP -d $TARGET_DIR
 
-# 将xray文件重命名为xray-linux-amd64
-mv xray xray-linux-amd64
+# 重命名并移动文件
+echo "更新文件..."
+mv -f ${TARGET_DIR}/xray ${TARGET_DIR}/xray-linux-amd64
+mv -f ${TARGET_DIR}/geoip.dat ${TARGET_DIR}/geosite.dat $TARGET_DIR
 
-# 将xray-linux-amd64、geoip.dat和geosite.dat文件移动到/usr/local/x-ui/bin目录中
-mv -f xray-linux-amd64 geoip.dat geosite.dat /usr/local/x-ui/bin/
-
-# 删除ZIP文件以节省空间
-rm Xray-linux-64.zip
+# 清理ZIP文件
+rm $XRAY_ZIP
 
 echo "Xray-core更新完成。"
