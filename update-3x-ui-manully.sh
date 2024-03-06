f4e9c1cf23251424c7e0800b3098079023e1240f
Jaya <gujiakai28@gmail.com>
2024-03-06 16:23:40 +0800

    Update update-3x-ui-manully.sh

--- update-3x-ui-manully.sh
+++ update-3x-ui-manully.sh
@@ -41,10 +41,7 @@ rm -rf x-ui-linux-amd64.tar.gz
 # 手动更新xray内核
 # 定义目标目录
 TARGET_DIR="/usr/local/x-ui/bin"
-XRAY_ZIP="${TARGET_DIR}/Xray-linux-64.zip"
-
-# 确保目标目录存在
-mkdir -p $TARGET_DIR
+XRAY_ZIP="Xray-linux-64.zip"
 
 # 获取最新的Xray-core发布的ZIP文件URL
 XRAY_LATEST_RELEASE_URL=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep browser_download_url | grep 'Xray-linux-64.zip' | cut -d '"' -f 4)
@@ -67,6 +64,6 @@ echo "更新文件..."
 mv -f ${TARGET_DIR}/xray ${TARGET_DIR}/xray-linux-amd64
 
 # 清理ZIP文件
-rm $XRAY_ZIP
+rm ${TARGET_DIR}/${XRAY_ZIP}
 
 echo "Xray-core更新完成。"
