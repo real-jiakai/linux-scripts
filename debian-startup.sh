f2a958774e217027f27ebff610ce6964321ed5a5
Jaya <gujiakai28@gmail.com>
2024-04-22 19:28:10 +0800

    Update debian-startup.sh

--- debian-startup.sh
+++ debian-startup.sh
@@ -3,6 +3,9 @@
 # 更新软件包列表 && 更新所有软件
 apt update && apt upgrade -y
 
+# 安装时间同步软件
+apt install ntp ntpdate -y
+
 # 将新的默认队列调度算法设置为 "fq"
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 # 将新的TCP拥塞控制算法设置为 "bbr"
